b02fc4ec4ca97884331829121a8a1479faf180b3
asra <asra@netcompany.com>
2017-06-19 21:12:22 +0200

    [asras] third time's the charm

--- FinanceManager.DataAccess.Tests/RepositoryTests.cs
+++ FinanceManager.DataAccess.Tests/RepositoryTests.cs
@@ -106,6 +106,7 @@ namespace FinanceManager.DataAccess.Tests
             var testvar3 = newdick.OrderBy(e => e.Key.Item1).ToList();
             var etst = "";
             var okwdpokawd = "";
+            var pwoida = "";
             var waijdjiw = testlist.OrderBy(e => e).Select(e => e + Guid.NewGuid().ToString()).ToList();
         }
         //[TestMethod]
