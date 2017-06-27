6d0dfc44a3960d1ce41dfbf237f2c21d35bd22c8
Explomatic <morten.madsen.92@gmail.com>
2017-06-27 21:00:23 +0200

    Explomatic: Added UnitTest project for DataManager

---
+++ FinanceManager.BusinessLogic.Tests/DataManagerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using FinanceManager.DataAccess;
+
+namespace FinanceManager.BusinessLogic.Tests
+{
+    [TestClass]
+    public class BusinessLogicTests
+    {
+        private IRepository<Dictionary<(string, DateTime), List<double>>> _repo = new Repository<Dictionary<(string, DateTime), List<double>>>();
+        private IDataManager _DataManager = new DataManager();
+
+        [TestMethod]
+        public void TestAddData()
+        {
+            string name = "Bananer";
+            string date = DateTime.Parse("10/10/2017").ToString();
+            string amount = "10";
+
+            string _testinfo = _DataManager.AddData(date, name, amount);
+
+            Assert.IsTrue(String.Equals(_testinfo,"(name, date) tuple did not exist. Created (key, value) pair and initialized list."));
+
+            _testinfo = _DataManager.AddData(date, name, amount);
+
+            Assert.IsTrue(String.Equals(_testinfo, "(name, date) tuple already existed. Amount appended to list."));
+        }
+
+
+    }
+}
---
+++ FinanceManager.BusinessLogic.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FinanceManager.BusinessLogic.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FinanceManager.BusinessLogic.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0b5cf55f-e03f-436d-ad84-de8aa6b70259")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
